bdebc90985ddb479ef8c48748f3f071beb8251cb
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-23 11:36:10 -0700

    update AppContect.jsx

--- Frontend/src/Context/AppContect.jsx
+++ Frontend/src/Context/AppContect.jsx
@@ -7,8 +7,16 @@ export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [user, setuser] = useState(null);
   const [isSeller, setisSeller] = useState(false);
-
-  const value = { navigate, user, setuser, setisSeller, isSeller };
+const [showUserlogin, setshowUserlogin] = useState(false);
+  const value = {
+    navigate,
+    user,
+    setuser,
+    setisSeller,
+    isSeller,
+    showUserlogin,
+    setshowUserlogin,
+  };
 
   // Fix: Make sure return and JSX are on the same line or use parentheses
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
