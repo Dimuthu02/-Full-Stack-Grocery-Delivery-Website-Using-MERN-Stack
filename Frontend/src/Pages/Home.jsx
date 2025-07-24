cf840212edb18d0f18e562a9980b8f599b5e3a8a
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-24 00:28:07 -0700

    import main banner

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainBanner from '../Components/MainBanner'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+      <MainBanner />
+      
+    </div>
+  )
+}
+ className='mt-10'
+export default Home
