c5f76c821daab2edd6263d21bd937e67ba527fc1
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-24 00:27:06 -0700

    update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <NavBar />
-    
+<div>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    </Routes>
+  </div>    
     </div>
-  )
+  
+)
 }
 
 export default App
